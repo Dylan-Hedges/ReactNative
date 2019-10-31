1679980b11b755f39a8ff1f17a0bda6d5bdd73ae
DylanHedges <d.hedges1@hotmail.co.uk>
2019-10-31 07:51:14 -0400

    Added <FlatList /> to display a new color on screen each time the button is pressed (used when you want to create an array of elements)

--- Test/src/screens/ColorScreen.js
+++ Test/src/screens/ColorScreen.js
@@ -1,16 +1,24 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, Button} from 'react-native';
+import {View, StyleSheet, Button, FlatList} from 'react-native';
 
 const ColorScreen = () => {
   //Initalises Functional Component state - stores colors
   const [colors, setColors] = useState([]);
-  //Returns colors on screen - [...colors, randomRgb()] takes a copy of the colors array, pastes it into the new array, then addeds a new entry by calling randomRgb() function
+  //Returns colors on screen - [...colors, randomRgb()] takes a copy of the colors array, pastes it into the new array, then addeds a new entry by calling randomRgb() function, <FlatList /> used when you have an array of elements you want to display on screen
   return(
     <View>
       <Button title="add a color" onPress={()=>{
         setColors([...colors, randomRgb()]);
       }}/>
-      <View style={{height: 100, width: 100, backgroundColor: randomRgb()}} />
+      <FlatList
+        keyExtractor={item => item}
+        data={colors}
+        renderItem={({item})=>{
+          return(
+            <View style={{height: 100, width: 100, backgroundColor: item}} />
+          );
+        }}
+        />
     </View>
   )
 }
