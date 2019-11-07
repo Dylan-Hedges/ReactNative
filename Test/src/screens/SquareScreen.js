b0b3b776a973ab7b9f615c598ce41cacd184bee7
DylanHedges <d.hedges1@hotmail.co.uk>
2019-11-07 07:09:13 -0500

    Updated naming conventions (type: & payload) for action object to match community standards

--- Test/src/screens/SquareScreen.js
+++ Test/src/screens/SquareScreen.js
@@ -7,20 +7,20 @@ const COLOR_INCREMENT = 15;
 
 //Reducer function - returns an object to be used as the new state (copies the state object, pastes it into a new object, updates it, returns updated object to be used as new state )
 const reducer = (state, action) => {
-  switch(action.colorToChange){
-    case 'red':
+  switch(action.type){
+    case 'change_red':
       //Updates state for red - first performs validation check using a ternary operator, if the current value of red (state.red) + the incrementor (action.amount) is > 255 or < 0 then do nothing & just return state, otherwise update state, ""...state" makes a copy of state obect, pastes it to the new { } object, replaces the red: property with "red: state.red + action.amount" (does not mutate state directly), the object is then returned which acts as the new state
-      return state.red + action.amount > 255 || state.red + action.amount < 0
+      return state.red + action.payload > 255 || state.red + action.payload < 0
       ? state
-      : {...state, red: state.red + action.amount};
-    case 'green':
-      return state.green + action.amount > 255 || state.green + action.amount < 0
+      : {...state, red: state.red + action.payload};
+    case 'change_green':
+      return state.green + action.payload > 255 || state.green + action.payload < 0
       ? state
-      :  {...state, green: state.green + action.amount};
-    case 'blue':
-      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+      :  {...state, green: state.green + action.payload};
+    case 'change_blue':
+      return state.blue + action.payload > 255 || state.blue + action.payload < 0
       ? state
-      : {...state, blue: state.blue + action.amount };
+      : {...state, blue: state.blue + action.payload };
     default:
       return state;
   }
@@ -35,16 +35,16 @@ const SquareScreen = () => {
   return(
     <View>
       <ColorCounter
-       onIncrease={() => dispatch({colorToChange: 'red', amount: COLOR_INCREMENT})}
-       onDecrease={() => dispatch({colorToChange: 'red', amount: -1 * COLOR_INCREMENT})}
+       onIncrease={() => dispatch({type: 'change_red', payload: COLOR_INCREMENT})}
+       onDecrease={() => dispatch({type: 'change_red', payload: -1 * COLOR_INCREMENT})}
        color='Red '/>
       <ColorCounter
-        onIncrease={() => dispatch({colorToChange: 'blue', amount: COLOR_INCREMENT})}
-        onDecrease={() => dispatch({colorToChange: 'blue', amount: -1 * COLOR_INCREMENT})}
+        onIncrease={() => dispatch({type: 'change_blue', payload: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({type: 'change_blue', payload: -1 * COLOR_INCREMENT})}
         color='Blue'/>
       <ColorCounter
-        onIncrease={() => dispatch({colorToChange: 'green', amount: COLOR_INCREMENT})}
-        onDecrease={() => dispatch({colorToChange: 'green', amount: -1 * COLOR_INCREMENT})}
+        onIncrease={() => dispatch({type: 'change_green', payload: COLOR_INCREMENT})}
+        onDecrease={() => dispatch({type: 'change_green', payload: -1 * COLOR_INCREMENT})}
         color='Green'/>
        <View  style={{height: 150, width: 150, backgroundColor:`rgb(${red},${green},${blue})`}}/>
     </View>
