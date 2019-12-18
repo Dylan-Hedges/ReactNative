e81404e34c40c69298a68d09996f934106c2b25a
DylanHedges <d.hedges1@hotmail.co.uk>
2019-12-18 06:51:51 -0500

    Added reuseable component that displays restaurants on screen

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,8 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   //Creates a term state for this component - used by the search bar to make searches, setTerm lets us update the term state
@@ -20,6 +21,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null }
       <Text>We have found {results.length} results</Text>
+      <ResultsList title="Cost Effective"/>
+      <ResultsList title="Bit Pricier"/>
+      <ResultsList title="Big Spender" />
     </View>
   );
 };
