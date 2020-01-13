747fad1e336ff58170fe61a24c1b37b9e67eb84b
DylanHedges <d.hedges1@hotmail.co.uk>
2020-01-13 07:53:40 -0500

    Added hide category if no restaurants returned functionality - if no restaurants are returned for a category e.g Big Spender then return null (return nothing on screen) otherwise the title will show with no results

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -5,6 +5,12 @@ import ResultsDetail from './ResultsDetail.js';
 
 //Renders list of returned restaurants on screen - when tapped opens up restaurant details in a new component (passes the business id to the ResultsDetail component which uses the id to exectue an API request to the Yelp API)
 const ResultsList = ({title, results, navigation}) => {
+  //If there are no restaurants for a category (e.g Big Spender) then dont return anything for that category (null) - category is not displayed on screen, otherwise a title is displayed with nothing underneath
+  if(!results.length){
+    return null;
+  }
+
+  //Displays a list of restaurants on screen when making a search
   return(
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
